2fe3660e808d1c3b7de325998702f4e6bd707d66
jorgeferv <42806662+jorgeferv@users.noreply.github.com>
2026-03-01 15:57:28 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* sw.js - MacroControlAPP
    Estrategia: network-first para index, cache-first para estáticos.
 */
-const CACHE = 'mcapp-cache-v1.5.9-r57c16-p1';
+const CACHE = 'mcapp-cache-v1.5.9-r57c17';
 const ASSETS = [
   './',
   './index.html',
